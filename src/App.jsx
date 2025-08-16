6ce0aabd2cb6e628a420bcd9592cea84d2263f54
JuanEndl <juanendl-_@hotmail.com>
2025-08-16 15:32:42 -0300

    api + estructura de la tabla

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import BusquedaComponentes from './components/BusquedaComponentes'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
       <BusquedaComponentes />
     </>
   )
