bf073f8324b7cdb96167aca1f3e2de43b7a3ccbf
JuanEndl <juanendl-_@hotmail.com>
2025-08-16 15:19:25 -0300

    creacion de los hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-
 import './App.css'
+import BusquedaComponentes from './components/BusquedaComponentes'
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <BusquedaComponentes />
     </>
   )
 }
---
+++ src/components/BusquedaComponentes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BusquedaComponentes = () => {
+  return (
+    <div>BusquedaComponentes</div>
+  )
+}
+
+export default BusquedaComponentes
