34af72bfefc606242e8ae3346eb24bba39fc2d11
JuanEndl <juanendl-_@hotmail.com>
2025-08-28 20:28:03 -0300

    modal editar modificacion 2

--- src/components/BusquedaComponentes.jsx
+++ src/components/BusquedaComponentes.jsx
@@ -188,10 +188,13 @@ const BusquedaComponentes = () => {
 
       {/* Modal editar*/}
       {modalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center">
+        <div className="fixed inset-0 flex items-center justify-center ">
           <div className="bg-white p-6 rounded-lg shadow-lg w-96">
             <h2 className="text-lg font-bold mb-4">Editar precio de compra</h2>
-            <input type="number" value={nuevoPrecio} onChange={(e) => setNuevoPrecio(e.target.value)} placeholder="Ingresar numero" className="w-full p-2 border rounded mb-4" />
+            <p>Nombre del producto</p>
+            <p className="mb-2 font-bold">{productoSeleccionado.descripcion}</p>
+            <p className="mb-2 font-bold text-red-800 rounded-lg">Precio anterior ${productoSeleccionado.precioCompra}</p>
+            <input type="number" value={nuevoPrecio} onChange={(e) => setNuevoPrecio(e.target.value)} placeholder="Ingresar precio nuevo" className="p-2 border rounded mb-4"/>
             <div className="flex justify-center space-x-2">
               <button onClick={guardarCambios} className="px-4 py-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Guardar
