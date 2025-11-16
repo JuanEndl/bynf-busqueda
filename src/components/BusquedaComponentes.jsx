f0a0ea5362329facc69e40a0347f06ac77241355
JuanEndl <juanendl-_@hotmail.com>
2025-11-16 20:14:49 -0300

    arreglo de botonos modal Eliminar

--- src/components/BusquedaComponentes.jsx
+++ src/components/BusquedaComponentes.jsx
@@ -386,8 +386,8 @@ const BusquedaComponentes = () => {
           </ul>
 
             <div className="flex justify-end mt-6 space-x-3">
+              <button className="text-white bg-green-700 hover:bg-green-800 rounded-lg px-5 py-2.5" disabled={seleccionados.length === 0} onClick={eliminarProductosSeleccionados}>Aceptar</button>
               <button className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center" onClick={() => setModalEliminarOpen(false)}>Cancelar</button>
-              <button className="text-white bg-red-600 hover:bg-red-800 rounded-lg px-5 py-2.5" disabled={seleccionados.length === 0} onClick={eliminarProductosSeleccionados}>Eliminar</button>
             </div>
           </div>
         </div>
